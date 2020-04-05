cefaf61b4d7a8777dd6ab2c8675c9b77052d5b95
zhang874010534 <z874010534@vip.qq.com>
2020-04-05 14:17:17 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,90 @@
+import {
+  request
+} from "../../request/request.js"
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    swiperList: [],
+    catesList:[]
+  },
+  // 获取轮播图数据
+  getSwiperList() {
+    request({
+        url: "https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata"
+      })
+      .then(result => {
+        this.setData({
+          swiperList: result.data.message
+        })
+      })
+  },
+  getCateList(){
+    wx.request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems',
+      success:(res)=>{
+        this.setData({
+          catesList:res.data.message
+        })
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getSwiperList()
+    this.getCateList()
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
